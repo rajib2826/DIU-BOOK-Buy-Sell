dbee0b424a75fa1b4e66465f4f76e503362fe620
rajib2826 <rajib15-2826@diu.edu.bd>
2022-11-06 21:40:01 +0600

    change added new price fill

--- src/data/perk.js
+++ src/data/perk.js
@@ -1,31 +1,31 @@
 const perks = [
   {
-    name: 'Free returns',
+    name: "Free returns",
     imageUrl:
-      'https://tailwindui.com/img/ecommerce/icons/icon-returns-light.svg',
+      "https://tailwindui.com/img/ecommerce/icons/icon-returns-light.svg",
     description:
-      'Not what you expected? Place it back in the parcel and attach the pre-paid postage stamp.',
+      "Not what you expected? Place it back in the parcel and attach the pre-paid postage stamp.",
   },
   {
-    name: 'Same day delivery',
+    name: "Same day delivery",
     imageUrl:
-      'https://tailwindui.com/img/ecommerce/icons/icon-calendar-light.svg',
+      "https://tailwindui.com/img/ecommerce/icons/icon-calendar-light.svg",
     description:
-      'We offer a delivery service that has never been done before. Checkout today and receive your products within hours.',
+      "We offer a delivery service that has never been done before. Checkout today and receive your products within hours.",
   },
   {
-    name: 'All year discount',
+    name: "All year discount",
     imageUrl:
-      'https://tailwindui.com/img/ecommerce/icons/icon-gift-card-light.svg',
+      "https://tailwindui.com/img/ecommerce/icons/icon-gift-card-light.svg",
     description:
       'Looking for a deal? You can use the code "ALLYEAR" at checkout and get money off all year round.',
   },
   {
-    name: 'For the planet',
+    name: "For the education",
     imageUrl:
-      'https://tailwindui.com/img/ecommerce/icons/icon-planet-light.svg',
+      "https://tailwindui.com/img/ecommerce/icons/icon-planet-light.svg",
     description:
-      'We’ve pledged 1% of sales to the preservation and restoration of the natural environment.',
+      "We’ve pledged 1% of sales to the preservation and restoration of the natural environment.",
   },
 ];
 
--- src/page/Post.jsx
+++ src/page/Post.jsx
@@ -43,110 +43,133 @@ const Post = ({ open, setOpen }) => {
                               careful what you share.
                             </p>
                           </div>
-
-                          <div className='mt-6 grid grid-cols-1 gap-y-6 gap-x-4 sm:grid-cols-6'>
-                            <div className='sm:col-span-4'>
-                              <label
-                                htmlFor='username'
-                                className='block text-sm font-medium text-gray-700'
-                              >
-                                Book Name
-                              </label>
-                              <div className='mt-1 flex rounded-md shadow-sm'>
-                                <input
-                                  type='text'
-                                  name='username'
-                                  id='username'
-                                  className='block w-full rounded-md border-gray-300 shadow-sm focus:border-indigo-500 focus:ring-indigo-500 sm:text-sm'
-                                />
-                              </div>
-                            </div>
-
-                            <div className='sm:col-span-6'>
-                              <label
-                                htmlFor='about'
-                                className='block text-sm font-medium text-gray-700'
-                              >
-                                Book Description
-                              </label>
-                              <div className='mt-1'>
-                                <textarea
-                                  id='about'
-                                  name='about'
-                                  rows={3}
-                                  className='block w-full rounded-md border-gray-300 shadow-sm focus:border-indigo-500 focus:ring-indigo-500 sm:text-sm'
-                                  defaultValue={''}
-                                />
-                              </div>
-                              <p className='mt-2 text-sm text-gray-500'>
-                                Write a few sentences about the book.
-                              </p>
-                            </div>
-
-                            <div className='sm:col-span-6'>
-                              <label
-                                htmlFor='cover-photo'
-                                className='block text-sm font-medium text-gray-700'
-                              >
-                                Book photo
-                              </label>
-                              <div className='mt-1 flex justify-center rounded-md border-2 border-dashed border-gray-300 px-6 pt-5 pb-6'>
-                                <div className='space-y-1 text-center'>
-                                  <svg
-                                    className='mx-auto h-12 w-12 text-gray-400'
-                                    stroke='currentColor'
-                                    fill='none'
-                                    viewBox='0 0 48 48'
-                                    aria-hidden='true'
-                                  >
-                                    <path
-                                      d='M28 8H12a4 4 0 00-4 4v20m32-12v8m0 0v8a4 4 0 01-4 4H12a4 4 0 01-4-4v-4m32-4l-3.172-3.172a4 4 0 00-5.656 0L28 28M8 32l9.172-9.172a4 4 0 015.656 0L28 28m0 0l4 4m4-24h8m-4-4v8m-12 4h.02'
-                                      strokeWidth={2}
-                                      strokeLinecap='round'
-                                      strokeLinejoin='round'
-                                    />
-                                  </svg>
-                                  <div className='flex text-sm text-gray-600'>
-                                    <label
-                                      htmlFor='file-upload'
-                                      className='relative cursor-pointer rounded-md bg-white font-medium text-indigo-600 focus-within:outline-none focus-within:ring-2 focus-within:ring-indigo-500 focus-within:ring-offset-2 hover:text-indigo-500'
-                                    >
-                                      <span>Upload a file</span>
-                                      <input
-                                        id='file-upload'
-                                        name='file-upload'
-                                        type='file'
-                                        className='sr-only'
-                                      />
-                                    </label>
-                                    <p className='pl-1'>or drag and drop</p>
-                                  </div>
-                                  <p className='text-xs text-gray-500'>
-                                    PNG, JPG, GIF up to 10MB
-                                  </p>
+                          <form className="mx-4">
+                            <div className='mt-6 grid grid-cols-1 gap-y-4 gap-x-4 sm:grid-cols-6'>
+                              <div className='sm:col-span-6'>
+                                <label
+                                  htmlFor='username'
+                                  className='block text-sm font-medium text-gray-700'
+                                >
+                                  Book Name
+                                </label>
+                                <div className='mt-1 flex rounded-md shadow-sm'>
+                                  <input
+                                    type='text'
+                                    name='username'
+                                    id='username'
+                                    className='block w-full rounded-md border-gray-300 shadow-sm focus:border-indigo-500 focus:ring-indigo-500 sm:text-sm'
+                                  />
                                 </div>
                               </div>
-                            </div>
 
-                            <div className='sm:col-span-4'>
-                              <label
-                                htmlFor='price'
-                                className='block text-sm font-medium text-gray-700'
-                              >
-                                Book Price
-                              </label>
-                              <div className='mt-1 flex rounded-md shadow-sm'>
-                                <input
-                                  type='number'
-                                  name='price'
-                                  id='price'
-                                  className='block w-full rounded-md border-gray-300 shadow-sm focus:border-indigo-500 focus:ring-indigo-500 sm:text-sm'
-                                />
+                              <div className='sm:col-span-6'>
+                                <label
+                                  htmlFor='about'
+                                  className='block text-sm font-medium text-gray-700'
+                                >
+                                  Book Description
+                                </label>
+                                <div className='mt-1'>
+                                  <textarea
+                                    id='about'
+                                    name='about'
+                                    rows={3}
+                                    className='block w-full rounded-md border-gray-300 shadow-sm focus:border-indigo-500 focus:ring-indigo-500 sm:text-sm'
+                                    defaultValue={''}
+                                  />
+                                </div>
+                                <p className='mt-2 text-sm text-gray-500'>
+                                  Write a few sentences about the book.
+                                </p>
                               </div>
+
+                              <div className='sm:col-span-6'>
+                                <label
+                                  htmlFor='cover-photo'
+                                  className='block text-sm font-medium text-gray-700'
+                                >
+                                  Book photo
+                                </label>
+                                <div className='mt-1 flex justify-center rounded-md border-2 border-dashed border-gray-300 px-6 pt-5 pb-6'>
+                                  <div className='space-y-1 text-center'>
+                                    <svg
+                                      className='mx-auto h-12 w-12 text-gray-400'
+                                      stroke='currentColor'
+                                      fill='none'
+                                      viewBox='0 0 48 48'
+                                      aria-hidden='true'
+                                    >
+                                      <path
+                                        d='M28 8H12a4 4 0 00-4 4v20m32-12v8m0 0v8a4 4 0 01-4 4H12a4 4 0 01-4-4v-4m32-4l-3.172-3.172a4 4 0 00-5.656 0L28 28M8 32l9.172-9.172a4 4 0 015.656 0L28 28m0 0l4 4m4-24h8m-4-4v8m-12 4h.02'
+                                        strokeWidth={2}
+                                        strokeLinecap='round'
+                                        strokeLinejoin='round'
+                                      />
+                                    </svg>
+                                    <div className='flex text-sm text-gray-600'>
+                                      <label
+                                        htmlFor='file-upload'
+                                        className='relative cursor-pointer rounded-md bg-white font-medium text-indigo-600 focus-within:outline-none focus-within:ring-2 focus-within:ring-indigo-500 focus-within:ring-offset-2 hover:text-indigo-500'
+                                      >
+                                        <span>Upload a file</span>
+                                        <input
+                                          id='file-upload'
+                                          name='file-upload'
+                                          type='file'
+                                          className='sr-only'
+                                        />
+                                      </label>
+                                      <p className='pl-1'>or drag and drop</p>
+                                    </div>
+                                    <p className='text-xs text-gray-500'>
+                                      PNG, JPG, GIF up to 10MB
+                                    </p>
+                                  </div>
+                                </div>
+                              </div>
+
+                              <div className='sm:col-span-6'>
+                                <label
+                                  htmlFor='price'
+                                  className='block text-sm font-medium text-gray-700'
+                                >
+                                  Book Orginal Price
+                                </label>
+                                <div className='mt-1 flex rounded-md shadow-sm'>
+                                  <input
+                                    type='number'
+                                    name='price'
+                                    id='price'
+                                    className='block w-full rounded-md border-gray-300 shadow-sm focus:border-indigo-500 focus:ring-indigo-500 sm:text-sm'
+                                  />
+                                </div>
+                              </div>
+
+                              <div className='sm:col-span-6'>
+                                <label
+                                  htmlFor='price'
+                                  className='block text-sm font-medium text-gray-700'
+                                >
+                                  Book selling Price
+                                </label>
+                                <div className='mt-1 flex rounded-md shadow-sm'>
+                                  <input
+                                    type='number'
+                                    name='price'
+                                    id='price'
+                                    className='block w-full rounded-md border-gray-300 shadow-sm focus:border-indigo-500 focus:ring-indigo-500 sm:text-sm'
+                                  />
+                                </div>
+                              </div>
+
+
                             </div>
-                          </div>
+                          </form>
+
                         </div>
                       </div>
+
+
 
                       <div className='pt-5'>
                         <div className='flex justify-end'>
